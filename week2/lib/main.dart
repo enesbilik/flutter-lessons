66ec46232090add0c7aa643ae922a29910dbc807
Enes Bilik <enesbilik8@gmail.com>
2022-03-31 00:00:55 +0300

    week2 init

---
+++ week2/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Welcome to Flutter'),
+        ),
+        body: Center(
+          child: Column(
+            children: const [
+              Text('Hello World'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
